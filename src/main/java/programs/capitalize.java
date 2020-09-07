81d2d554e6c39724abf531172c0c1f064844c201
rajesh g <rajeshg.gmx@gmail.com>
2020-09-07 08:17:37 +0530

    added capitalize word program

---
+++ src/main/java/programs/capitalize.java
@@ -0,0 +1,34 @@
+package programs;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class capitalize {
+
+    public static void main(String[] args) {
+
+        System.out.println("Captitalizing first word");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        String capitalized = getCapitalized(input);
+        System.out.println(capitalized);
+    }
+
+    private static String getCapitalized(String input) {
+
+        String[] trimmed = input.split(" ");
+
+        StringBuilder sb = new StringBuilder();
+        for (String text : trimmed) {
+            Character firstChar = text.charAt(0);
+            if(!Character.isUpperCase(firstChar)){
+               sb = sb.append(Character.toUpperCase(firstChar)).append(text.substring(1)).append(" ");
+        } else {
+                sb = sb.append(text).append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
